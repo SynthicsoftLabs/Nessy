12ee85b44e5c982f47b7d68e22f782046ea6908a
Synthic-Synth-01 <synthicsoftlabs@gmail.com>
2026-08-24 17:29:44 -0500

    fix: remove MIT daemon header

--- crates/bowserd/src/main.rs
+++ crates/bowserd/src/main.rs
@@ -1,5 +1,5 @@
 // Copyright 2026 Synthicsoft Labs LLC
-// Licensed under the Apache License, Version 2.0 and the MIT License.
+// Licensed under the Apache License, Version 2.0.
 
 use std::sync::Arc;
 use tokio::signal;
